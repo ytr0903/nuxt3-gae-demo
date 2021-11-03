240fbf1963b26a914735ecde9a782ae34c63b99c
ytr0903 <35318519+ytr0903@users.noreply.github.com>
2021-11-03 18:43:16 +0900

    layouts + add bulma

---
+++ project/layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <div class="section">
+    <div class="container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "assets/style.scss";
+</style>
