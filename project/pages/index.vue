3354ac4ed013822dab420a192a9cd5d6b4392cb8
ytr0903 <35318519+ytr0903@users.noreply.github.com>
2021-11-03 18:43:31 +0900

    index.vue

---
+++ project/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button class="button" @click="count++">Click Me.</button>
+    <p>
+      Single: {{ count }}<br />
+      Double: {{ doubled }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const count = ref(0);
+
+const doubled = computed(() => count.value * 2);
+</script>
